a809054ff3d0f8cdeefc841be3ce4495f051335d
Nikhil Sharma <159870552+Nikhil9Sharma@users.noreply.github.com>
2026-05-07 17:59:55 +0530

    Add Server.js file

--- server.js
+++ server.js
@@ -1,37 +1,13 @@
-const express = require("express");
-const mysql = require("mysql2");
-const cors = require("cors");
-require("dotenv").config();
+import express from 'express';
 
 const app = express();
 
-app.use(cors());
-app.use(express.json());
-
-// Database connection
-const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-});
-
-db.connect((err) => {
-  if (err) {
-    console.log("Database connection failed:", err);
-  } else {
-    console.log("Connected to MySQL");
-  }
-});
-
-// Routes
 app.get("/", (req, res) => {
-  res.send("Server running successfully 🚀");
-});
+  res.send("Backend is working !");
+})
+const PORT = process.env.PORT || 5000;
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
-app.get("/", (req, res) => {
-    res.send("Backend is working!");
-});
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server at http://localhost:${PORT}`);
+})
